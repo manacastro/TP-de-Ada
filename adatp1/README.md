8d2a628ca018c3dc07bfe2f190f47030e2470d2e
Mariana Castro <mb.castro17@gmail.com>
2019-08-29 11:27:37 -0300

    tp1

---
+++ adatp1/README.md
@@ -0,0 +1,3 @@
+## Trabajo prático N°1
+
+Se puede ver [acá](https://manacastro.github.io/adatp1/).
---
+++ adatp1/tp-results/result.js
@@ -0,0 +1,48 @@
+let observations = [
+  {label:'Identación', score:9},
+  {label:'Maquetado', score:8},
+  {label:'Css', score:7},
+  {label:'Responsive', score:9},
+  {label:'Interacciones', score:9},
+  {label:'Consigna', score:9},
+  {label:'Apreciativa', score:8}
+]
+let finalScore = 0;
+observations.forEach( e => {finalScore += e.score})
+
+const styledScore = e => {
+  let span = document.createElement('span')
+  span.textContent = e
+  span.className = e >= 7 ? 'check' : 'alert'
+  return span
+}
+
+const createElements = () => {
+  let container = document.createElement('div')
+  container.className = 'tp-result-container'
+
+  let strong = document.createElement('strong')
+  strong.textContent = Math.round((finalScore / observations.length)*10) / 10
+
+  let title = document.createElement('h3')
+  title.textContent = 'Aprobado'
+  title.appendChild(strong)
+  container.appendChild(title)
+  
+  let observationsList = document.createElement('ul')
+  container.appendChild(observationsList)
+
+  observations.forEach(e => {
+    let item = document.createElement('li')
+    item.textContent = e.label+': '
+    item.appendChild(styledScore(e.score))
+    observationsList.appendChild(item)
+  })
+
+  return container
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  let body = document.getElementsByTagName("BODY")[0] 
+  body.appendChild(createElements())
+});
